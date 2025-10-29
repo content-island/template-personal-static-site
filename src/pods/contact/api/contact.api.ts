5658457a63d605a58453d9e590d9808431648e61
Leticia de la Osa <deletidev@gmail.com>
2025-10-29 14:04:40 +0100

    refactor: enhance layout components with reverseAside prop and integrate ContactPod for improved contact section

---
+++ src/pods/contact/api/contact.api.ts
@@ -0,0 +1,7 @@
+import client from '#lib/client.ts';
+import type { ContactSection } from './contact.api-model';
+
+export const getContactSection = async () =>
+  await client.getContent<ContactSection>({
+    id: '6901fd053b04f4ec2b218b84',
+  });
