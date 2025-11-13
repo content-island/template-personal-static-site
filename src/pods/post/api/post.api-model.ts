1f4e787e303366592927f127cf47afea70b08a3a
Manuel Dorado Serrano <manueldoradoserrano@manuel-dorado.local>
2025-11-13 14:43:11 +0100

    fix: remove minRead property from BlogSubheader interface

--- src/pods/post/api/post.api-model.ts
+++ src/pods/post/api/post.api-model.ts
@@ -4,5 +4,4 @@ export interface BlogSubheader {
   lastUpdate: string; // Stores the date in ISO 8601 format. For example: 2021-09-10T19:30:00.000Z
   backLabel: string;
   publishedLabel: string;
-  minRead: string;
 }
